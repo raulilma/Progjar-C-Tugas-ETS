bb1a64f938cb457c0301bbef8e5bb302c1524009
Raul Ilma Rajasa <raulilmarajasa@gmail.com>
2023-04-01 16:17:35 +0700

    fix: change port to 20000

--- server_thread_http_secure.py
+++ server_thread_http_secure.py
@@ -66,7 +66,7 @@ class Server(threading.Thread):
 		threading.Thread.__init__(self)
 
 	def run(self):
-		self.my_socket.bind(('0.0.0.0', 8443))
+		self.my_socket.bind(('0.0.0.0', 20000))
 		self.my_socket.listen(1)
 		while True:
 			self.connection, self.client_address = self.my_socket.accept()
